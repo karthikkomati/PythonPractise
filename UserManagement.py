4412d5398bf4959f983831d20169a7d9ef8e754a
karthikkomati <53406991+karthikkomati@users.noreply.github.com>
2020-06-15 18:23:47 +0100

    Add files via upload

---
+++ UserManagement.py
@@ -0,0 +1,84 @@
+import mysql.connector
+
+def getConnection():
+    db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database = "usermanagement"
+    )
+    return db
+
+def create(firstname,lastname,username,password,email):
+    db = getConnection()
+    mycursor = db.cursor()
+    q = "insert into users(Firstname,Lastname,Username,Password,Email) values(%s,%s,%s,%s,%s)"
+    #q = "insert into users(Firstname,Lastname,Username,Password,Email) values('{}','{}','{}','{}','{}')".format(firstname,lastname,username,password,email)
+    mycursor.execute(q,(firstname,lastname,username,password,email))
+    db.commit()
+    db.close
+    
+def readAll():
+    db = getConnection()
+    mycursor = db.cursor()
+
+    mycursor.execute("select * from users")
+    for x in mycursor:
+        print(x)
+    db.close()
+
+def read(col,val):
+    db = getConnection()
+    mycursor = db.cursor()
+
+    q = "select * from users where {} = %s".format(col)
+    mycursor.execute(q,(val,))
+    for x in mycursor:
+        print(x)
+    db.close()
+
+def deleteUser(uname):
+    db = getConnection()
+    mycursor = db.cursor()
+    q = "delete from users where username = %s"
+    
+    mycursor.execute(q,(uname,))
+    db.commit()
+    db.close()
+    
+    
+def delete(col,val):
+    db = getConnection()
+    mycursor = db.cursor()
+    q = "delete from users where {} = %s".format(col)
+
+    mycursor.execute(q,(val,))
+    db.commit()
+    db.close()
+
+def updateAll(uname,firstname,lastname,username,password,email):
+    db = getConnection()
+    mycursor = db.cursor()
+    q = "Update users set firstname = %s,lastname = %s,username = %s ,password = %s,email= %s where username = %s"
+
+    mycursor.execute(q,(firstname,lastname,username,password,email,uname))
+    db.commit()
+    db.close()
+
+def update(uname,col,val):
+    db = getConnection()
+    mycursor = db.cursor()
+    q = "Update users set {} = %s where username = %s".format(col)
+
+    mycursor.execute(q,(val,uname))
+    db.commit()
+    db.close()
+
+#create("test3","test3","test3","test3","test3")
+#read("username","test2")
+#deleteUser("test3")
+#delete("firstname","test3")
+#updateAll("update","update1","update","update","update","update",)
+#update("update","password","pass")
+#readAll()
+
